e08efbe71c749c0510d99ca89da112e4e63eac0d
Clemmer <clemmece@jmu.edu>
2023-05-03 10:24:43 -0400

    Adding logging to a file

--- netmiko_ssh_test.py
+++ netmiko_ssh_test.py
@@ -5,9 +5,42 @@ from netmiko import *
 import traceback
 import getpass
 import sys
+import time
+import os
+import pwd
+import grp
+from shutil import make_archive
+import logging
+
+
+#TODO
+# Clean up imports
+# Add enumeration for device types
+# ?
+# profit
 
 
 date = date.today()
+timestr = time.strftime("%Y-%m-%d-%H%M%S")
+did_error = False
+
+logging.basicConfig(filename=f'{date}-run.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s-%(levelname)s:%(message)s')
+
+try:
+    device_file = sys.argv[1]
+except IndexError as err:
+    logging.error('No device file specified')
+    logging.exception(err)
+    sys.exit()
+
+
+
+try:
+    os.mkdir(f"/tftp/{timestr}")
+except FileExistsError as err:
+    logging.error("TFTP archive directory already exists")
+    logging.exception(err)
+    sys.exit()
 
 switch={
         'device_type':'',
@@ -15,68 +48,103 @@ switch={
         'username':'',
         'password':'',
         'port':22,
-        'secret':''
+        'secret':'',
+        'conn_timeout':20,
+        'banner_timeout':20,
+        'auth_timeout':20
         }
 
 
-#get creds and other stuff
+#get creds and TFTP server destination
 switch['username']=input("User name: ")
 switch['password']=getpass.getpass(prompt='Enter user password: ')
 switch['secret']=getpass.getpass(prompt='Enter enable password: ')
 tftp_server = input("Enter TFTP server ip: ")
 
 
-with open('switch-list.txt') as f:
-        for line in f:
-            l = line.split(',')
+try:
+    with open(device_file) as f:
+            for line in f:
+                l = line.split(',')
 
-            start_time = datetime.now()
-            switch_ip=l[1].strip()
-            switch_device_type = l[0].strip()
-            output = ''
+                start_time = datetime.now()
+                switch_ip=l[1].strip()
+                switch_device_type = l[0].strip()
+                output = ''
 
-            switch['device_type'] = switch_device_type
-            switch['ip']=switch_ip
+                switch['device_type'] = switch_device_type
+                switch['ip']=switch_ip
 
-            #ssh connect
-            try:
-                print(f"Connecting to {switch_ip}...")
-                ssh_connect = ConnectHandler(**switch)
-            except NetmikoAuthenticationException as err:
-                print("SSH-CONNECT: Authentication Failed")
-            except NetmikoTimeoutException as err:
-                print("SSH-CONNECT: Timeout Exception")
-            except err:
-                print(f"Connection to {switch_ip} failed ...")
-                print("Uknown error: " + str(err))
+                #ssh connect
+                try:
+                    print(f"Connecting to {switch_ip}...")
+                    logging.info(f"Connecting to {switch_ip}...")
+                    ssh_connect = ConnectHandler(**switch)
+                except NetmikoAuthenticationException as err:
+                    logging.error("SSH-CONNECT: Authentication Failed")
+                    logging.exception(err)
+                except NetmikoTimeoutException as err:
+                    logging.error("SSH-CONNECT: Timeout Exception")
+                    logging.exception(err)
+                except err:
+                    logging.error(f"Connection to {switch_ip} failed ...")
+                    logging.exception(err)
 
-            #perform switch management
-            command_string=f"copy running-config tftp://{tftp_server}/{switch_ip}-run-backup.txt"
-
-            if switch_device_type == "cisco_ios":
-                print("TO DO: HANDLE CISCO")
-            else:
+                #perform switch management
                 try:
                     with ConnectHandler(**switch) as net_connect:
                         try:
                             print(f"Connected to {switch_ip}, copying config ...")
-                            net_connect.enable()
-                            output += net_connect.send_command_timing(command_string, strip_prompt=False, strip_command=False)
-                            output += net_connect.send_command_timing("y", strip_prompt=False, strip_command=False, read_timeout=90)
-                            print(f"Copy complete for {switch_ip}")
-                        except NetMikoTimeoutException as e:
-                            print(f"TFTP copy failed for {switch_ip}.")
-                        except err:
-                            print("Unknown error: " + str(err))
 
-                    ssh_connect.disconnect()
-                    print("Disconnected")
+                            if switch_device_type == "cisco_ios":
+                                net_connect.enable()
+                                cmd_list = [
+                                        f"copy running-config tftp://{tftp_server}/{switch_ip}-run-backup.txt",
+                                        "\n",
+                                        "\n"
+                                        ]
+                                output += net_connect.send_multiline_timing(cmd_list)
+                            elif switch_device_type == "dell_os6":
+                                net_connect.enable()
+                                command_string=f"copy running-config tftp://{tftp_server}/{switch_ip}-run-backup.txt"
+                                output += net_connect.send_command_timing(command_string, strip_prompt=False, strip_command=False)
+                                output += net_connect.send_command_timing("y", strip_prompt=False, strip_command=False, read_timeout=90)
+                            elif switch_device_type == "juniper_junos":
+                                cmd_list = [
+                                        "start shell",
+                                        "cd /config",
+                                        "tftp",
+                                        f"connect {tftp_server}",
+                                        f"put juniper.conf.gz {switch_ip}-conf-backup.gz",
+                                        "\x04",
+                                        "exit"
+                                        ]
+                                output += net_connect.send_multiline_timing(cmd_list)
+                            print(f"Copy complete for {switch_ip}")
+                            logging.info(f"Copy complete for {switch_ip}")
+                        except NetMikoTimeoutException as e:
+                            logging.error(f"TFTP copy failed for {switch_ip}.")
+                            logging.exception(err)
+                        except err:
+                            logging.error("Unknown error: " + str(err))
+                            logging.exception(err)
+
+                        ssh_connect.disconnect()
+                        print("Disconnected")
+                        logging.info("Disconnected")
                 except NetmikoAuthenticationException as err:
-                    print(f"Authentication failure for {switch_ip}")
+                    logging.error(f"Authentication failure for {switch_ip}")
+                    logging.exception(err)
                 except NetmikoTimeoutException as err:
-                    print(f"Timeout connencting to {switch_ip}")
+                    logging.error(f"Timeout connencting to {switch_ip}")
+                    logging.exception(err)
                 finally:
                     end_time = datetime.now()
                     print(f"Exec time for {switch_ip}: {end_time - start_time}")
+                    logging.info(f"Exec time for {switch_ip}: {end_time - start_time}")
                     print()
-                                                                                             
+except FileNotFoundError as e:
+    logging.error(f"{device_file} does not exist")
+    logging.exception(e)
+
+make_archive(f'{date}','zip',root_dir=None, base_dir=f"/tftp/{timestr}")
